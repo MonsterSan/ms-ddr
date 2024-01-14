32cc0b422371d3329ac422dab39516ff642ffeae
MonsterSan <410906739@qq.com>
2024-01-14 12:49:11 +0800

    afnb test

--- train.py
+++ train.py
@@ -18,6 +18,7 @@ from lib.models.bisenetv2 import BiSeNetV2
 from lib.models.bisenetv1_without_arm import BiSeNetV1_without_Arm
 from lib.models.bisenetv1_without_ffm import BiSeNetV1_without_ffm
 from lib.models.bisenetv1_with_aspp import BiSeNetV1_with_aspp
+from lib.models.bisenetv1_afnb import BiSeNetV1_afnb
 
 from torch.optim.lr_scheduler import PolynomialLR
 from torch.nn.modules.loss import CrossEntropyLoss
@@ -31,7 +32,7 @@ from utils.save_weight import save_weights
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str,
-                    default='bisenetv1', help='model name')
+                    default='bisenetv1_afnb', help='model name')
 # D:\\data\\Crack_Forest_paddle\\Crack_Forest_paddle
 # /home/user/data/lumianliefeng/Crack_Forest_paddle
 parser.add_argument('--dataset_root', type=str,
@@ -98,6 +99,10 @@ if __name__ == "__main__":
         loss_weights = [1, 1, 1]
     elif args.model == 'bisenetv1_with_aspp':
         model = BiSeNetV1_with_aspp(args.num_classes)
+        losses = [OhemCrossEntropyLoss(), OhemCrossEntropyLoss(), OhemCrossEntropyLoss()]
+        loss_weights = [1, 1, 1]
+    elif args.model == 'bisenetv1_afnb':
+        model = BiSeNetV1_afnb(args.num_classes)
         losses = [OhemCrossEntropyLoss(), OhemCrossEntropyLoss(), OhemCrossEntropyLoss()]
         loss_weights = [1, 1, 1]
     elif args.model == 'bisenetv2':
