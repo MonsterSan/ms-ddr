6087b93ce544b7dc9b76800d395a252eb9d23567
MonsterSan <463113913@qq.com>
2024-02-06 13:19:46 +0800

    noag_casv2_noffm_sac completed

--- test.py
+++ test.py
@@ -17,6 +17,7 @@ from lib.models.bisenetv1_noarm_global2aspp import BiSeNetV1_noarm_global2aspp
 from lib.models.bisenetv1_noag_CaS import BiSeNetV1_noag_CaS
 from lib.models.bisenetv1_noag_CaSv2 import BiSeNetV1_noag_CaSv2
 from lib.models.bisenetv1_noag_CaSv2_noffm import BiSeNetV1_noag_CaSv2_noffm
+from lib.models.bisenetv1_noag_CaSv2_noffm_SaC import BiSeNetV1_noag_CaSv2_noffm_SaC
 
 from torch.nn.modules.loss import CrossEntropyLoss
 from lib.losses.ohem_cross_entropy_loss import OhemCrossEntropyLoss
@@ -26,9 +27,9 @@ from lib.utils.confusion_matrix import ConfusionMatrix
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str,
-                    default='bisenetv1_noag_casv2_noffm', help='model name')
+                    default='bisenetv1_noag_casv2_noffm_sac', help='model name')
 parser.add_argument('--log_path', type=str,
-                    default='./run/bisenetv1_noag_casv2_noffm_20240205_180716', help='log path')
+                    default='./run/bisenetv1_noag_casv2_noffm_sac_20240205_224953', help='log path')
 parser.add_argument('--checkpoint_type', type=str,
                     default='best_miou', help="best_miou or last or min_loss")
 # D:\\data\\Crack_Forest_paddle\\Crack_Forest_paddle
@@ -79,6 +80,8 @@ if __name__ == '__main__':
             model = BiSeNetV1_noag_CaSv2(args.num_classes)
         elif args.model == 'bisenetv1_noag_casv2_noffm':
             model = BiSeNetV1_noag_CaSv2_noffm(args.num_classes)
+        elif args.model == 'bisenetv1_noag_casv2_noffm_sac':
+            model = BiSeNetV1_noag_CaSv2_noffm_SaC(args.num_classes)
         else:
             raise KeyError("unknown model: {}".format(args.model))
 
