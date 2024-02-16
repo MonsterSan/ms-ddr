e11f0f70842666a3d0ee74d3bf098ddada71ab7e
MonsterSan <410906739@qq.com>
2024-02-16 13:25:11 +0800

    noarmglobal_taspp test

--- train.py
+++ train.py
@@ -25,6 +25,7 @@ from lib.models.bisenetv1_noag_CaSv2_noffm_SaC import BiSeNetV1_noag_CaSv2_noffm
 from lib.models.bisenetv1_noglobal import BiSeNetV1_noglobal
 from lib.models.bisenetv1_noarmglobal_add import BiSeNetV1_noarmglobal_add
 from lib.models.bisenetv1_noag_attention import BiSeNetV1_noag_attention
+from lib.models.bisenetv1_noag_taspp import BiSeNetV1_noag_taspp
 
 from torch.optim.lr_scheduler import PolynomialLR
 from torch.nn.modules.loss import CrossEntropyLoss
@@ -38,7 +39,7 @@ from utils.save_weight import save_weights
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str,
-                    default='bisenetv1_noag_attention', help='model name')
+                    default='bisenetv1_noag_taspp', help='model name')
 # D:\\data\\Crack_Forest_paddle\\Crack_Forest_paddle
 # /home/user/data/lumianliefeng/Crack_Forest_paddle
 parser.add_argument('--dataset_root', type=str,
@@ -104,6 +105,8 @@ if __name__ == "__main__":
             model = BiSeNetV1_noglobal(args.num_classes)
         elif args.model == 'bisenetv1_noarmglobal':
             model = BiSeNetV1_noarmglobal(args.num_classes)
+        elif args.model == 'bisenetv1_noag_taspp':
+            model = BiSeNetV1_noag_taspp(args.num_classes)
         elif args.model == 'bisenetv1_noag_attention':
             model = BiSeNetV1_noag_attention(args.num_classes)
         elif args.model == 'bisenetv1_noarmglobal_add':
