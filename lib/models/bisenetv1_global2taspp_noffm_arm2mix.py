44a6b762392f2c2a0afcb52021542c267a23d58d
MonsterSan <410906739@qq.com>
2024-03-20 14:39:28 +0800

    bisenetv1_global2taspp_noffm_arm2mix test

--- lib/models/bisenetv1_global2taspp_noffm_arm2mix.py
+++ lib/models/bisenetv1_global2taspp_noffm_arm2mix.py
@@ -12,6 +12,22 @@ from lib.models.resnet import Resnet18
 from torch.nn import BatchNorm2d
 
 
+class MixAtten(nn.Module):
+    def __init__(self, inc):
+        super(MixAtten, self).__init__()
+
+        self.conv1 = nn.Conv2d(2, 1, 1, 1, 0, bias=False)
+        self.bn = BatchNorm2d(inc)
+
+    def forward(self, x, y):
+        global_x = torch.mean(x, dim=(2, 3), keepdim=True)
+        global_y = torch.mean(y, dim=(2, 3), keepdim=True)
+        atten = self.conv1(torch.cat([global_x, global_y], dim=1))
+        atten = self.bn(atten)
+        atten = atten.sigmoid()
+        return x * atten, y * (1-atten)
+
+
 class ConvBNReLU(nn.Module):
 
     def __init__(self, in_chan, out_chan, ks=3, stride=1, padding=1, *args, **kwargs):
@@ -98,7 +114,7 @@ class ASPP(nn.Module):
         super(ASPP, self).__init__()
         if not atrous_rates:
             atrous_rates = [6, 12, 18]
-        self.split = in_channels//5
+        self.split = in_channels // 5
         self.inc = in_channels
         # 1*1 branch
         self.branch1 = nn.Sequential(
@@ -134,8 +150,8 @@ class ASPP(nn.Module):
         # avgpool branch
         self.branch5 = nn.Sequential(
             nn.AdaptiveAvgPool2d(1),
-            nn.Conv2d(in_channels-self.split*4, in_channels-self.split*4, 1, bias=False),
-            nn.BatchNorm2d(in_channels-self.split*4),
+            nn.Conv2d(in_channels - self.split * 4, in_channels - self.split * 4, 1, bias=False),
+            nn.BatchNorm2d(in_channels - self.split * 4),
             nn.ReLU()
         )
 
@@ -146,7 +162,8 @@ class ASPP(nn.Module):
             nn.Dropout(0.5))
 
     def forward(self, x):
-        x1, x2, x3, x4, x5 = torch.split(x, [self.split,self.split,self.split,self.split,self.inc-4*self.split], dim=1)
+        x1, x2, x3, x4, x5 = torch.split(x, [self.split, self.split, self.split, self.split, self.inc - 4 * self.split],
+                                         dim=1)
         out1 = self.branch1(x1)
         out2 = self.branch2(x2)
         out3 = self.branch3(x3)
@@ -162,24 +179,26 @@ class ContextPath(nn.Module):
     def __init__(self, *args, **kwargs):
         super(ContextPath, self).__init__()
         self.resnet = Resnet18()
-        self.arm16 = AttentionRefinementModule(256, 128)
-        self.arm32 = AttentionRefinementModule(128, 128)
+        self.conv16 = ConvBNReLU(256, 128, ks=1, stride=1, padding=0, bias=False)
+        # self.arm32 = AttentionRefinementModule(128, 128)
         self.conv_head32 = ConvBNReLU(128, 128, ks=3, stride=1, padding=1)
         self.conv_head16 = ConvBNReLU(128, 128, ks=3, stride=1, padding=1)
         self.conv_avg = ConvBNReLU(512, 128, ks=1, stride=1, padding=0)
         self.up32 = nn.Upsample(scale_factor=2.)
         self.up16 = nn.Upsample(scale_factor=2.)
-        self.aspp = ASPP(512,128)
+        self.aspp = ASPP(512, 128)
+        self.mix = MixAtten(128)
         self.init_weight()
 
     def forward(self, x):
         feat8, feat16, feat32 = self.resnet(x)
         feat32_aspp = self.aspp(feat32)
-        feat32_arm = self.arm32(feat32_aspp)
-        feat32_up = self.up32(feat32_arm)
+        # feat32_arm = self.arm32(feat32_aspp)
+        feat32_up = self.up32(feat32_aspp)
         feat32_up = self.conv_head32(feat32_up)
 
-        feat16_conv = self.arm16(feat16)
+        feat16_conv = self.conv16(feat16)
+        feat32_up, feat16_conv = self.mix(feat32_up, feat16_conv)
         feat16_sum = feat16_conv + feat32_up
         feat16_up = self.up16(feat16_sum)
         feat16_up = self.conv_head16(feat16_up)
@@ -238,60 +257,13 @@ class SpatialPath(nn.Module):
         return wd_params, nowd_params
 
 
-class FeatureFusionModule(nn.Module):
-    def __init__(self, in_chan, out_chan, *args, **kwargs):
-        super(FeatureFusionModule, self).__init__()
-        self.convblk = ConvBNReLU(in_chan, out_chan, ks=1, stride=1, padding=0)
-        ## use conv-bn instead of 2 layer mlp, so that tensorrt 7.2.3.4 can work for fp16
-        self.conv = nn.Conv2d(out_chan,
-                              out_chan,
-                              kernel_size=1,
-                              stride=1,
-                              padding=0,
-                              bias=False)
-        self.bn = nn.BatchNorm2d(out_chan)
-        self.init_weight()
-
-    def forward(self, fsp, fcp):
-        fcat = torch.cat([fsp, fcp], dim=1)
-        feat = self.convblk(fcat)
-        atten = torch.mean(feat, dim=(2, 3), keepdim=True)
-        atten = self.conv(atten)
-        atten = self.bn(atten)
-        #  atten = self.conv1(atten)
-        #  atten = self.relu(atten)
-        #  atten = self.conv2(atten)
-        atten = atten.sigmoid()
-        feat_atten = torch.mul(feat, atten)
-        feat_out = feat_atten + feat
-        return feat_out
-
-    def init_weight(self):
-        for ly in self.children():
-            if isinstance(ly, nn.Conv2d):
-                nn.init.kaiming_normal_(ly.weight, a=1)
-                if not ly.bias is None: nn.init.constant_(ly.bias, 0)
-
-    def get_params(self):
-        wd_params, nowd_params = [], []
-        for name, module in self.named_modules():
-            if isinstance(module, (nn.Linear, nn.Conv2d)):
-                wd_params.append(module.weight)
-                if not module.bias is None:
-                    nowd_params.append(module.bias)
-            elif isinstance(module, nn.modules.batchnorm._BatchNorm):
-                nowd_params += list(module.parameters())
-        return wd_params, nowd_params
-
-
-class BiSeNetV1_global2taspp(nn.Module):
+class BiSeNetV1_global2taspp_noffm_arm2mix(nn.Module):
 
     def __init__(self, n_classes, aux_mode='train', *args, **kwargs):
-        super(BiSeNetV1_global2taspp, self).__init__()
+        super(BiSeNetV1_global2taspp_noffm_arm2mix, self).__init__()
         self.cp = ContextPath()
         self.sp = SpatialPath()
-        self.ffm = FeatureFusionModule(256, 256)
-        self.conv_out = BiSeNetOutput(256, 256, n_classes, up_factor=8)
+        self.conv_out = BiSeNetOutput(128, 128, n_classes, up_factor=8)
         self.aux_mode = aux_mode
         if self.aux_mode == 'train':
             self.conv_out16 = BiSeNetOutput(128, 64, n_classes, up_factor=8)
@@ -302,7 +274,7 @@ class BiSeNetV1_global2taspp(nn.Module):
         H, W = x.size()[2:]
         feat_cp8, feat_cp16 = self.cp(x)
         feat_sp = self.sp(x)
-        feat_fuse = self.ffm(feat_sp, feat_cp8)
+        feat_fuse = feat_sp + feat_cp8
 
         feat_out = self.conv_out(feat_fuse)
         if self.aux_mode == 'train':
@@ -327,7 +299,7 @@ class BiSeNetV1_global2taspp(nn.Module):
         wd_params, nowd_params, lr_mul_wd_params, lr_mul_nowd_params = [], [], [], []
         for name, child in self.named_children():
             child_wd_params, child_nowd_params = child.get_params()
-            if isinstance(child, (FeatureFusionModule, BiSeNetOutput)):
+            if isinstance(child, (BiSeNetOutput)):
                 lr_mul_wd_params += child_wd_params
                 lr_mul_nowd_params += child_nowd_params
             else:
@@ -337,7 +309,7 @@ class BiSeNetV1_global2taspp(nn.Module):
 
 
 if __name__ == "__main__":
-    net = BiSeNetV1_global2taspp(2)
+    net = BiSeNetV1_global2taspp_noffm_arm2mix(2)
     net.cuda()
     net.eval()
     in_ten = torch.randn(16, 3, 512, 512).cuda()
